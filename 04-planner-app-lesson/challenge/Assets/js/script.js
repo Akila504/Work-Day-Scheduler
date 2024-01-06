1a277d43e2559dc4e3d3f5c4ab636d0a2e4a8844
Akila D.A <akiladahanayake@outlook.com>
2024-01-06 20:40:19 +0000

    I added the timeblocks html

---
+++ 04-planner-app-lesson/challenge/Assets/js/script.js
@@ -0,0 +1,17 @@
+//First part in HTML I can hard code the time block html
+//i.e from 9 am to 5pm 
+
+
+
+//Second part once the HTML is done// 
+
+// we will have to select all the time blocks that we need i.e using one ID tag 
+// omce the time blocks are selected we need to be able to add in a description to each time slot 
+
+
+
+//if one of the time blocks is clicked then we want to be able to input text
+//if the current time block time is either greater or less than the current time we need to color code
+//each time block should have a save button appended to it
+// the save button should have a click event when clicked will save the user input into local storage.
+// the events should be present even on page refresh 
